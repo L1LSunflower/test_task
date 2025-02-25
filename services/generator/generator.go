ca8910483fda92ac9e6cd28723c2f62cd4301824
l1l ketamine <l1lsvnfl0w3r@gmail.com>
2025-02-26 02:12:02 +0500

    feat: добавил функционал для generator

--- services/generator/generator.go
+++ services/generator/generator.go
@@ -15,11 +15,37 @@ func NewSymbols(rowsCount int, gameTapes []symbols.Symbols) *Symbols {
 }
 
 func (s *Symbols) Generate(rng rng.RNG) (symbols.Reels, error) {
-	// todo: implement me
-	return nil, nil
+	const numberOfReels = 5
+
+	gameField := make(symbols.Reels, s.rowsCount)
+	for i := range gameField {
+		gameField[i] = make([]int, numberOfReels)
+	}
+
+	for colI := 0; colI < numberOfReels; colI++ {
+		symbolTape := s.gameTapes[colI]
+
+		startIndex := rng.Random(0, uint32(len(symbolTape)))
+		reelColumnSymbols := s.GetReelSymbols(colI, int(startIndex))
+
+		for rowI := 0; rowI < s.rowsCount; rowI++ {
+			gameField[rowI][colI] = reelColumnSymbols[rowI]
+		}
+	}
+
+	return gameField, nil
 }
 
 func (s *Symbols) GetReelSymbols(reelIndex int, rowIndex int) symbols.Symbols {
-	// todo: implement me
-	return nil
+	symbolTape := s.gameTapes[reelIndex]
+	tapeLength := len(symbolTape)
+	reelSymbols := make(symbols.Symbols, 3)
+
+	for i := 0; i < s.rowsCount; i++ {
+		indexInTape := (rowIndex + i) % tapeLength
+		symbol := symbolTape[indexInTape]
+		reelSymbols[i] = symbol
+	}
+
+	return reelSymbols
 }
